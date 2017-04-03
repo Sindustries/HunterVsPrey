d9697308c24fc2c476d1159a585c044020e95f6c
Jay <jleach92@hotmail.co.uk>
2017-04-03 22:42:39 +0100

    Lowered Unconscious change
    From 25 to 10%

--- cfgMaster.hpp
+++ cfgMaster.hpp
@@ -20,7 +20,7 @@ class HVP {
 /* SPAWN SETTINGS */
 
 	HVPSpawnType = 0;					//0:Random, 1:Ground, 2:HALO, 3:Helicopter, 4:Drop Pod
-	groundChanceUnconscious = 25;		//Chance of being unconscious during "Ground" spawn
+	groundChanceUnconscious = 10;		//Chance of being unconscious during "Ground" spawn
 
 /* EVENT SETTINGS */
 
